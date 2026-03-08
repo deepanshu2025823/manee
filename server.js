1a6c4b63acf28c74814f3ffa2c357cbaf132be57
Webeside Technology <webesidetechnologyindia@gmail.com>
2026-03-08 10:11:09 +0530

    update

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.prepare().then(() => {
           [chatId, 'user', prompt]
         );
 
-        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.0" });
         const result = await model.generateContentStream(prompt);
 
         let fullManeeResponse = "";
